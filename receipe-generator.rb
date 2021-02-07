175b111a520044a5271099dc785985d1ba93b56d
Mathias <r3morce@users.noreply.github.com>
2021-02-07 13:01:01 +0100

    Add unit

--- receipe-generator.rb
+++ receipe-generator.rb
@@ -2,17 +2,18 @@
 load "class Ingredient.arb"
 load "class Receipe.arb"
 
-# ingredients
-chicken = Ingredient.new("Hühnerbrust", 150)
-rice = Ingredient.new("Reis", 300)
-paprika = Ingredient.new("Paprika", 2)
-eggs = Ingredient.new("Eier", 2)
-noodles = Ingredient.new("Nudeln", 180)
-banana = Ingredient.new("Banane", 1)
+# Ingredients
+chicken = Ingredient.new("Hühnerbrust", 150, "g")
+rice = Ingredient.new("Reis", 300, "g")
+paprika = Ingredient.new("Paprika", 2, "Stück")
+eggs = Ingredient.new("Eier", 2, "Stück")
+noodles = Ingredient.new("Nudeln", 180, "g")
+banana = Ingredient.new("Banane", 1, "Stück")
+stock = Ingredient.new("Brühe", 200, "ml ")
 
 allIngredients = [chicken, rice, paprika, eggs, noodles, banana]
 
-# receipe
+# Receipes
 riceWithChicken = Receipe.new("Rice mit Shice", [chicken, rice])
 
 randomReceipes = Array.new
@@ -25,12 +26,29 @@ for i in 0..5
     randomReceipes[i] = Receipe.new(receipeName, randomIngredients)
 end
 
-puts randomReceipes.length 
+# Shopping list
+shoppingList = Hash.new
 
 randomReceipes.each do |receipe|
-    puts receipe.name
-    receipe.ingredients.each do |j|
-        puts j.name
+    receipe.ingredients.each do |ingredient|
+        if shoppingList[ingredient].nil?
+            shoppingList[ingredient] = ingredient.value
+        else 
+            shoppingList[ingredient] += ingredient.value
+        end
     end
 
+end
+
+# Output
+puts "Das sind deine Rezepte" 
+randomReceipes.each do |receipe|
+    puts "\t#{receipe.name}" 
+end
+
+puts ""
+
+puts "Das ist deine Einkaufsliste" 
+shoppingList.each do |key, value|
+    puts "\t#{key.name}: #{value} #{key.unit}" 
 end
